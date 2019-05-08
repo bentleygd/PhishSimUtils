a6d2c83dd602af57339d9c0a6a132d22c3d76e41
Gabriel Bentley <gbentley@24hourfit.com>
2019-05-08 14:23:15 -0700

    new file:   .gitignore 	new file:   lib/phishsim.py

---
+++ lib/phishsim.py
@@ -0,0 +1,18 @@
+from re import search
+
+
+class GetConfig:
+    """A configuration class for PhishSim integration"""
+    def __init__(self, file_location):
+        self.fl = file_location
+
+
+    def GetToken(self):
+        """Gets an API key from the config file"""
+        # Note to self: quit being lazy and write a function to get the
+        # API key from an encrypted file.
+        config_file = open(self.fl, 'r+b')
+        for line in config_file:
+            t_rgx = search(r'(^API_Key = )(\w+.+)', line)
+        config_file.close()
+        return t_rgx.group(2).strip()
