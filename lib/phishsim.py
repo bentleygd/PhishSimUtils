269fc8067dfb94a5936716d548e1f007d9bfa4b2
Gabriel Bentley <gbentley@24hourfit.com>
2019-05-15 13:10:37 -0700

    modified:   phishsim.py

--- lib/phishsim.py
+++ lib/phishsim.py
@@ -1,5 +1,6 @@
 from re import search
-
+# from ldap import initialize, SCOPE_SUBTREE
+from requests import get
 
 class GetConfig:
     """A configuration class for PhishSim integration"""
@@ -13,5 +14,44 @@ class GetConfig:
         config_file = open(self.fl, 'r+b')
         for line in config_file:
             t_rgx = search(r'(^API_Key = )(\w+.+)', line)
+            if t_rgx:
+                return t_rgx.group(2).strip()
         config_file.close()
-        return t_rgx.group(2).strip()
+
+class PhishSimUser:
+    """A class for PhishSim learners"""
+    def __init__(self, e_addr):
+        self.fname = str()
+        self.lname = str()
+        self.lid = str()
+        self.email = e_addr
+        self.phish_dates = []
+        self.entered_data_dates = []
+        self.phish_cnt = 0
+        self.entr_data_cnt = 0
+
+    def GetLearnerID(self, api_key):
+        url = 'https://securityiq.infosecinstitute.com/api/v1/learners'
+        token = api_key
+        headers = {'Accept': 'application/json', 
+                   'Authorization': 'Bearer ' + token}
+        request = url + '?' + 'email=' + self.email
+        response = get(request, headers = headers)
+        data = response.json().get('data')[0]
+        self.lid = data.get('id')
+
+    def GetLTE(self, api_key):
+        url = ('https://securityiq.infosecinstitute.com/api/v1/learners/' +
+               self.lid + '/timeline-events')
+        token = api_key
+        headers = {'Accept': 'application/json',
+                   'Authorization': 'Bearer' + token}
+        response = get(url, headers=headers)
+        data = response.json()
+        for element in data.get('data'):
+            if elemenet.get('type') == 'phished-learner':
+                self.phish_cnt = self.phish_cnt + 1
+                self.phish_dates.append(element.get('timestamp'))
+            elif element.get('type') == 'entered-data':
+                self.entr_data_cnt = self.entr_data_cnt + 1
+                self.entered_data_dates.append(element.get('timestamp'))
