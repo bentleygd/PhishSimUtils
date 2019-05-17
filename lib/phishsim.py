8dbc472af7748de6ee323e2014304bcc3bc5b3c2
bentleygd <47011516+bentleygd@users.noreply.github.com>
2019-05-17 14:37:19 -0700

    Working (#1)
    * 	new file:   .gitignore
    	new file:   lib/phishsim.py
    
    * 	modified:   .gitignore
    
    * 	deleted:    lib/phishsim.pyc
    
    * 	modified:   .gitignore
    
    * 	modified:   phishsim.py
    
    * 	modified:   phishsim.py
    
    * Fixed some linting bugs
    
    * Added LDAP/AD configurations.
    
    * Added CSV writing
    
    * 	modified:   phishsim.py
    
    * 	modified:   lib/phishsim.py
    
    * 	modified:   phishsim.py
    
    * Update README.md
    
    * 	new file:   coreutils.py
    	modified:   phishsim.py
    
    * 	modified:   .gitignore
    	modified:   lib/coreutils.py
    
    * 	modified:   lib/coreutils.py
    
    * 	modified:   coreutils.py

---
+++ lib/phishsim.py
@@ -0,0 +1,77 @@
+from requests import get
+from ldap import initialize, SCOPE_SUBTREE
+from csv import DictWriter
+
+
+class PhishSimUser:
+    """A class for PhishSim learners"""
+    def __init__(self, e_addr):
+        self.fname = str()
+        self.lname = str()
+        self.lid = str()
+        self.email = e_addr
+        self.phish_dates = []
+        self.entered_data_dates = []
+        self.phish_cnt = 0
+        self.entr_data_cnt = 0
+        self.l_data = {'First Name':self.fname,
+                       'Last Name':self.lname,
+                       'Email Address':self.email,
+                       'Phish Count':self.phish_cnt,
+                       'Entered Data Count':self.entr_data_cnt}
+
+    def GetLearnerID(self, api_key):
+        """Gets the learner ID based on the learner email."""
+        url = 'https://securityiq.infosecinstitute.com/api/v1/learners'
+        token = api_key
+        headers = {'Accept': 'application/json',
+                   'Authorization': 'Bearer ' + token}
+        request = url + '?' + 'email=' + self.email
+        response = get(request, headers=headers)
+        data = response.json().get('data')[0]
+        self.lid = data.get('id')
+        self.fname = data.get('first_name')
+        self.lname = data.get('last_name')
+
+    def GetLTE(self, api_key):
+        """Gets phished and entered data for a learner."""
+        url = ('https://securityiq.infosecinstitute.com/api/v1/learners/' +
+               self.lid + '/timeline-events')
+        token = api_key
+        headers = {'Accept': 'application/json',
+                   'Authorization': 'Bearer' + token}
+        response = get(url, headers=headers)
+        data = response.json()
+        for element in data.get('data'):
+            if element.get('type') == 'phished-learner':
+                self.phish_cnt = self.phish_cnt + 1
+                self.phish_dates.append(element.get('timestamp'))
+            elif element.get('type') == 'entered-data':
+                self.entr_data_cnt = self.entr_data_cnt + 1
+                self.entered_data_dates.append(element.get('timestamp'))
+
+
+def GetADMailUsers(ldap_url, bind_dn, passw, ous):
+    """Retrieves email addresses from AD."""
+    email_list = []
+    ldap_obj = initialize(ldap_url)
+    ldap_obj.simple_bind_s(bind_dn, passw)
+    for ou in ous:
+        user_data = (ldap_obj.search_s(ou, SCOPE_SUBTREE, 'mail=*', ['mail'],
+                     attrsonly=0))
+        for data in user_data:
+            email_list.append(data[1].get('mail')[0].lower())
+    return email_list
+
+
+def PhishSimCSV(field_names, f_obj, dict_list):
+    """Writes results to a CSV file."""
+    f_names = field_names
+    writer = DictWriter(f_obj, extrasaction='ignore',
+                        fieldnames=f_names, dialect='excel')
+    writer.writeheader()
+    for user in dict_list:
+        writer.writerow({'First Name': user.l_data.get('fname'),
+                         'Last Name': user.l_data.get('lname'),
+                         'Phished': user.l_data.get('phish_cnt'),
+                         'Entered Data': user.l_data.get('entr_data_cnt')})
